1e66c1dedba558e1225829b17605e594b7e760b4
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-02-22 17:03:57 +0530

    Update Posts.tsx

--- src/components/Post/Posts.tsx
+++ src/components/Post/Posts.tsx
@@ -19,21 +19,16 @@ import { Post, postState, PostVote } from "../../atoms/postsAtom";
 import PostItem from "./PostItem";
 import { useRouter } from "next/router";
 import usePosts from "../../hooks/usePosts";
-
 type PostsProps = {
   communityData?: Community;
   userId?: string;
   loadingUser: boolean;
 };
-
 const Posts: React.FC<PostsProps> = ({
   communityData,
   userId,
   loadingUser,
 }) => {
-  /**
-   * PART OF INITIAL SOLUTION BEFORE CUSTOM HOOK
-   */
   const [loading, setLoading] = useState(false);
   // const setAuthModalState = useSetRecoilState(authModalState);
   const router = useRouter();
@@ -41,134 +36,6 @@ const Posts: React.FC<PostsProps> = ({
   const { postStateValue, setPostStateValue, onVote, onDeletePost } = usePosts(
     communityData!
   );
-
-  /**
-   * USE ALL BELOW INITIALLY THEN CONVERT TO A CUSTOM HOOK AFTER
-   * CREATING THE [PID] PAGE TO EXTRACT REPEATED LOGIC
-   */
-  // const onVote = async (
-  //   event: React.MouseEvent<SVGElement, MouseEvent>,
-  //   post: Post,
-  //   vote: number
-  // ) => {
-  //   event.stopPropagation();
-  //   if (!userId) {
-  //     setAuthModalState({ open: true, view: "login" });
-  //     return;
-  //   }
-
-  //   const { voteStatus } = post;
-
-  //   // is this an upvote or a downvote?
-  //   // has this user voted on this post already? was it up or down?
-  //   const existingVote = postItems.postVotes.find(
-  //     (item: PostVote) => item.postId === post.id
-  //   );
-
-  //   try {
-  //     let voteChange = vote;
-  //     const batch = writeBatch(firestore);
-
-  //     // New vote
-  //     if (!existingVote) {
-  //       const newVote: PostVote = {
-  //         postId: post.id,
-  //         communityId: communityData.id!,
-  //         voteValue: vote,
-  //       };
-
-  //       const postVoteRef = doc(
-  //         collection(firestore, "users", `${userId}/postVotes`)
-  //       );
-
-  //       // Needed for frontend state since we're not getting resource back
-  //       newVote.id = postVoteRef.id;
-  //       batch.set(postVoteRef, {
-  //         postId: post.id,
-  //         communityId: communityData.id!,
-  //         voteValue: vote,
-  //       });
-
-  //       // Optimistically update state
-  //       setPostItems((prev) => ({
-  //         ...prev,
-  //         postVotes: [...prev.postVotes, newVote],
-  //       }));
-  //     }
-  //     // Removing existing vote
-  //     else {
-  //       // Used for both possible cases of batch writes
-  //       const postVoteRef = doc(
-  //         firestore,
-  //         "users",
-  //         `${userId}/postVotes/${existingVote.id}`
-  //       );
-
-  //       // Removing vote
-  //       if (existingVote.voteValue === vote) {
-  //         voteChange *= -1;
-
-  //         setPostItems((prev) => ({
-  //           ...prev,
-  //           postVotes: prev.postVotes.filter((item) => item.postId !== post.id),
-  //         }));
-  //         batch.delete(postVoteRef);
-  //       }
-  //       // Changing vote
-  //       else {
-  //         voteChange = 2 * vote;
-
-  //         batch.update(postVoteRef, {
-  //           voteValue: vote,
-  //         });
-  //         // Optimistically update state
-  //         const existingPostIdx = postItems.postVotes.findIndex(
-  //           (item) => item.postId === post.id
-  //         );
-  //         const updatedVotes = [...postItems.postVotes];
-  //         updatedVotes[existingPostIdx] = { ...existingVote, voteValue: vote };
-  //         setPostItems((prev) => ({
-  //           ...prev,
-  //           postVotes: updatedVotes,
-  //         }));
-  //       }
-  //     }
-
-  //     const postRef = doc(firestore, "posts", post.id);
-  //     batch.update(postRef, { voteStatus: voteStatus + voteChange });
-
-  //     /**
-  //      * Perform writes
-  //      * Could move state updates to after this
-  //      * but decided to optimistically update
-  //      */
-  //     await batch.commit();
-  //   } catch (error) {
-  //     console.log("onVote error", error);
-  //   }
-  // };
-
-  // const getUserPostVotes = async () => {
-  //   try {
-  //     const postVotesQuery = query(
-  //       collection(firestore, `users/${userId}/postVotes`),
-  //       where("communityId", "==", communityData.id)
-  //     );
-
-  //     const postVoteDocs = await getDocs(postVotesQuery);
-  //     const postVotes = postVoteDocs.docs.map((doc) => ({
-  //       id: doc.id,
-  //       ...doc.data(),
-  //     }));
-  //     setPostItems((prev) => ({
-  //       ...prev,
-  //       postVotes: postVotes as PostVote[],
-  //     }));
-  //   } catch (error) {
-  //     console.log("getUserPostVotes error", error);
-  //   }
-  // };
-
   const onSelectPost = (post: Post, postIdx: number) => {
     setPostStateValue((prev) => ({
       ...prev,
@@ -176,7 +43,6 @@ const Posts: React.FC<PostsProps> = ({
     }));
     router.push(`/r/${communityData?.id!}/comments/${post.id}`);
   };
-
   useEffect(() => {
     if (
       postStateValue.postsCache[communityData?.id!] &&
@@ -188,42 +54,10 @@ const Posts: React.FC<PostsProps> = ({
       }));
       return;
     }
-
     getPosts();
-    /**
-     * REAL-TIME POST LISTENER
-     * IMPLEMENT AT FIRST THEN CHANGE TO POSTS CACHE
-     *
-     * UPDATE - MIGHT KEEP THIS AS CACHE IS TOO COMPLICATED
-     *
-     * LATEST UPDATE - FOUND SOLUTION THAT MEETS IN THE MIDDLE
-     * CACHE POST DATA, BUT REMOVE POSTVOTES CACHE AND HAVE
-     * REAL-TIME LISTENER ON POSTVOTES
-     */
-    // const postsQuery = query(
-    //   collection(firestore, "posts"),
-    //   where("communityId", "==", communityData.id),
-    //   orderBy("createdAt", "desc")
-    // );
-    // const unsubscribe = onSnapshot(postsQuery, (querySnaption) => {
-    //   const posts = querySnaption.docs.map((post) => ({
-    //     id: post.id,
-    //     ...post.data(),
-    //   }));
-    //   setPostItems((prev) => ({
-    //     ...prev,
-    //     posts: posts as [],
-    //   }));
-    //   setLoading(false);
-    // });
-
-    // // Remove real-time listener on component dismount
-    // return () => unsubscribe();
   }, [communityData, postStateValue.postUpdateRequired]);
-
   const getPosts = async () => {
     console.log("WE ARE GETTING POSTS!!!");
-
     setLoading(true);
     try {
       const postsQuery = query(
@@ -247,9 +81,7 @@ const Posts: React.FC<PostsProps> = ({
     }
     setLoading(false);
   };
-
   console.log("HERE IS POST STATE", postStateValue);
-
   return (
     <>
       {loading ? (
