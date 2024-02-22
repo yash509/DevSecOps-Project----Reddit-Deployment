63e77bfafec233bfe8c5b58d37f7a82fdbf14f3e
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-02-22 16:50:51 +0530

    Update theme.ts

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,7 +1,6 @@
 import { extendTheme } from "@chakra-ui/react";
 import { Button } from "./button";
 import { Input } from "./input";
-
 export const theme = extendTheme({
   colors: {
     brand: {
