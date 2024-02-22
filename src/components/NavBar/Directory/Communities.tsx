017f8236f6b07152cc395e9a4820eaf0e4a73560
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-02-22 16:59:50 +0530

    Update Communities.tsx

--- src/components/NavBar/Directory/Communities.tsx
+++ src/components/NavBar/Directory/Communities.tsx
@@ -8,16 +8,13 @@ import { communityState } from "../../../atoms/communitiesAtom";
 import { auth } from "../../../firebase/clientApp";
 import CreateCommunityModal from "../../Modal/CreateCommunity";
 import MenuListItem from "./MenuListItem";
-
 type CommunitiesProps = {
   menuOpen: boolean;
 };
-
 const Communities: React.FC<CommunitiesProps> = ({ menuOpen }) => {
   const [user] = useAuthState(auth);
   const [open, setOpen] = useState(false);
   const mySnippets = useRecoilValue(communityState).mySnippets;
-
   return (
     <>
       <CreateCommunityModal
@@ -25,9 +22,8 @@ const Communities: React.FC<CommunitiesProps> = ({ menuOpen }) => {
         handleClose={() => setOpen(false)}
         userId={user?.uid!}
       />
-      {/* COULD DO THIS FOR CLEANER COMPONENTS */}
-      {/* <Moderating snippets={snippetState.filter((item) => item.isModerator)} />
-      <MyCommunities snippets={snippetState} setOpen={setOpen} /> */}
+      {}
+      {}
       {mySnippets.find((item) => item.isModerator) && (
         <Box mt={3} mb={4}>
           <Text pl={3} mb={1} fontSize="7pt" fontWeight={500} color="gray.500">
@@ -75,5 +71,4 @@ const Communities: React.FC<CommunitiesProps> = ({ menuOpen }) => {
     </>
   );
 };
-
 export default Communities;
