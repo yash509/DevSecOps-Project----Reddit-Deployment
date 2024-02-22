94b5f6a5da94381a5fa2fe954f4d31f61e99f49c
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-02-22 16:56:23 +0530

    Update OAuthButtons.tsx

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -2,12 +2,9 @@ import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { auth } from "../../../firebase/clientApp";
-
 type OAuthButtonsProps = {};
-
 const OAuthButtons: React.FC<OAuthButtonsProps> = () => {
   const [signInWithGoogle, _, loading, error] = useSignInWithGoogle(auth);
-
   return (
     <Flex direction="column" mb={4} width="100%">
       <Button
