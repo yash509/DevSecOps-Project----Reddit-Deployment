ab7d41ec9623ee4c3123b56d80bea4d5c907f13b
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-02-22 16:49:16 +0530

    Update directoryMenuAtom.ts

--- src/atoms/directoryMenuAtom.ts
+++ src/atoms/directoryMenuAtom.ts
@@ -1,7 +1,6 @@
 import { atom } from "recoil";
 import { IconType } from "react-icons";
 import { TiHome } from "react-icons/ti";
-
 export type DirectoryMenuItem = {
   displayText: string;
   link: string;
@@ -9,24 +8,20 @@ export type DirectoryMenuItem = {
   iconColor: string;
   imageURL?: string;
 };
-
 interface DirectoryMenuState {
   isOpen: boolean;
   selectedMenuItem: DirectoryMenuItem;
 }
-
 export const defaultMenuItem = {
   displayText: "Home",
   link: "/",
   icon: TiHome,
   iconColor: "black",
 };
-
 export const defaultMenuState: DirectoryMenuState = {
   isOpen: false,
   selectedMenuItem: defaultMenuItem,
 };
-
 export const directoryMenuState = atom({
   key: "directoryMenuState",
   default: defaultMenuState,
