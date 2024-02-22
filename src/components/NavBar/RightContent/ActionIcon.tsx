ac804bf0c82077ed98485bad8f4408b88419bc25
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-02-22 17:00:53 +0530

    Update ActionIcon.tsx

--- src/components/NavBar/RightContent/ActionIcon.tsx
+++ src/components/NavBar/RightContent/ActionIcon.tsx
@@ -1,12 +1,10 @@
 import { Flex, Icon } from "@chakra-ui/react";
 import React from "react";
-
 type ActionIcon = {
   icon: any;
   size: number;
   onClick?: any;
 };
-
 const ActionIcon: React.FC<ActionIcon> = ({ icon, size }) => {
   return (
     <Flex
